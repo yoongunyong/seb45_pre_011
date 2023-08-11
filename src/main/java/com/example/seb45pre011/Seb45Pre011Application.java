da7ca511a65f4f0996242d0f8af6aa17b05730d0
pyo8470 <rkswlska789@naver.com>
2023-08-11 16:48:32 +0900

    첫 세팅 #1

---
+++ src/main/java/com/example/seb45pre011/Seb45Pre011Application.java
@@ -0,0 +1,13 @@
+package com.example.seb45pre011;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb45Pre011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb45Pre011Application.class, args);
+	}
+
+}
